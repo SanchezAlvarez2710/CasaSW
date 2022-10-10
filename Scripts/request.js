ad06b3c62135f1a7f0808d536bdfd1166474cf7f
san7201 <simon_sanchez82192@elpoli.edu.co>
2022-10-10 14:26:25 -0500

    (style) finished request view styles
    - Added scroll bar to upgrade view interactivity and visibility
    - Delete cancel button
    - Fixed minor bugs respect content distribution

---
+++ Scripts/request.js
@@ -0,0 +1,12 @@
+﻿const form = document.getElementById("form-request");
+var select = document.getElementById('prodtype');
+
+function clean() {
+    form.reset();
+}
+
+function select() {
+    if (select.options[select.selectedIndex].text == 'Other') {
+        document.getElementById("pother").style.display = inline-block;
+    }
+}
