300b03e595a32ba57960b16301be7cd450958ba0
Miller Cano <miller_cano82201@elpoli.edu.co>
2022-10-14 12:42:53 -0500

    (Feat) Added Logout Controller

---
+++ Controllers/LOGOUTController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CasaSW.Controllers
+{
+    public class LOGOUTController : Controller
+    {
+        // GET: LOGOUT
+        public ActionResult Logout()
+        {
+            Session["usuario"] = null;
+            return RedirectToAction("Login", "LOGIN");
+        }
+    }
+}
